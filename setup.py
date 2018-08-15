d6b524016ba6e15e858b15f444a2f57203d28a12
Jarosław Śmiejczak <j.smiejczak@clearcode.cc>
2018-08-15 21:48:09 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -68,5 +68,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
